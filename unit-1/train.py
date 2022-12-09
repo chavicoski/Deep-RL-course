ae0c6b3310b069fe4997470854d22c2e9fd5ae61
chavicoski <alvaro.lopez.chilet@gmail.com>
2022-12-09 20:13:57 +0100

    Minor changes in train.py

--- unit-1/train.py
+++ unit-1/train.py
@@ -4,7 +4,6 @@ from hydra.utils import instantiate
 from omegaconf import DictConfig
 from stable_baselines3.common.base_class import BaseAlgorithm
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.monitor import Monitor
 
 
 @hydra.main(version_base=None, config_path="config", config_name="train")
@@ -24,7 +23,7 @@ def main(cfg: DictConfig):
         n_eval_episodes=cfg.n_eval_episodes,
         deterministic=cfg.deterministic,
     )
-    print(f"Evaluation results: {mean_reward=:.2f} +/- {std_reward}")
+    print(f"Evaluation results: {mean_reward=:.2f} +/- {std_reward:.2f}")
 
 
 if __name__ == "__main__":
