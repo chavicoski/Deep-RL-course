52c4a932ba5ec6d686a750fa7f45fec007baee6e
chavicoski <alvaro.lopez.chilet@gmail.com>
2022-12-14 22:11:16 +0100

    Fixed model_to_hub script for unit1-bonus (Huggy)

--- unit-1-bonus/model_to_hub.sh
+++ unit-1-bonus/model_to_hub.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Use the run id of the model to push. By default it should be a date (%Y-%m-%d_%H-%M-%S)
-export RUN_ID="2022-12-12_22-42-47"
+export RUN_ID="2022-12-13_16-30-16"
 # Use the repo id for your {user/organization}/{repo-name}
-export REPO_ID="chavicoski/ppo-Huggy-test"
+export REPO_ID="chavicoski/ppo-Huggy"
 # A message to show with the model description
-export COMMIT_MESSAGE="Test push with custom script"
+export COMMIT_MESSAGE="New version with 0.0001 lr"
 
 mlagents-push-to-hf \
-    --run-id=$RUN_ID \
-    --local-dir="./runs/train/${RUN_ID}" \
-    --repo-id=$REPO_ID \
-    --commit-message=$COMMIT_MESSAGE
+    --run-id="$RUN_ID" \
+    --local-dir="./runs/train/$RUN_ID" \
+    --repo-id="$REPO_ID" \
+    --commit-message="$COMMIT_MESSAGE"
